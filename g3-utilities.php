1dc72c54661f6e00b5d4fce16970946d6eec5e41
Amit Gupta <coolamit@gmail.com>
2022-02-28 20:15:19 +0530

    Updated version to v1.0.0

--- g3-utilities.php
+++ g3-utilities.php
@@ -3,13 +3,13 @@
 Plugin Name: G3 Utilities
 Plugin URI: https://www.goodglamm.com/
 Description: Collection of utility code/libraries for use with WordPress plugins/themes.
-Version: 0.1-beta
-Author: G3 Engineering, Amit Gupta
+Version: 1.0.0
+Author: Good Glamm Group Engineering, Amit Gupta
 License: GPL v2
 */
 
 define( 'G3_UTILITIES_ROOT', __DIR__ );
-define( 'G3_UTILITIES_VERSION', '0.1-beta' );
+define( 'G3_UTILITIES_VERSION', '1.0.0' );
 
 function g3_utilities_loader() : void {
 	require_once G3_UTILITIES_ROOT . '/dependencies.php';
