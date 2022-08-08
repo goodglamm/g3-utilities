52a827d2984e2d5c6a2e263c7811186725f803ff
Amit Gupta <coolamit@gmail.com>
2022-08-08 17:51:40 +0530

    Added `Factory` trait to implement Factory Pattern.

---
+++ src/classes/traits/trait-factory.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Trait to implement the Factory pattern in a class
+ *
+ * @author Amit Gupta
+ *
+ * @since  2022-08-04
+ */
+
+namespace G3\Utilities\Traits;
+
+trait Factory {
+
+	/**
+	 * Method to return a new object of the
+	 * current class.
+	 *
+	 * This is a variadic method which is able to accept
+	 * unspecified number of arguments. All of those arguments
+	 * are passed to the constructor of the class using this Trait
+	 * as individual arguments. Whether those arguments
+	 * are used or not depends on the class using this Trait.
+	 *
+	 * This method has been set as final intentionally,
+	 * because it is not meant to be overridden.
+	 *
+	 * @param array $args
+	 *
+	 * @return static
+	 */
+	final public static function get_instance( ...$args ) : self {
+
+		return new static( ...$args );
+
+	}
+
+}  // end trait
+
+//EOF
